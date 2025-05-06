68c1d3bf4b024595bd133d9d0f516b95a4ced9ce
rajatkumar <rajatprasad24@gmail.com>
2025-05-06 15:39:52 +0530

    Resolved the error in Annotation transformer

--- src/main/java/com/qa/opencart/listeners/AnnotationTransformer.java
+++ src/main/java/com/qa/opencart/listeners/AnnotationTransformer.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
 import org.testng.annotations.ITestAnnotation;
 
 import reactor.util.retry.Retry;
@@ -15,6 +16,6 @@ public class AnnotationTransformer implements IAnnotationTransformer {
 			Class testClass, 
 			Constructor testConstructor, 
 			Method testMethod) {
-		annotation.setRetryAnalyzer(Retry.class);
+		annotation.setRetryAnalyzer((Class<? extends IRetryAnalyzer>) Retry.class);
 	}
 }
