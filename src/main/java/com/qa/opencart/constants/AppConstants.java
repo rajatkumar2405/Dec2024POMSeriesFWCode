e31e4d56b782ab3a3bd2f694b9ca9a9052dfac89
rajatkumar <rajatprasad24@gmail.com>
2025-05-20 19:41:11 +0530

    reverting the failed test case

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class AppConstants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String HOME_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_FRACTION_URL = "route=account/login";
 	public static final String HOME_PAGE_FRACTION_URL = "route=account/account";
